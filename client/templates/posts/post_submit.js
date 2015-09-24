1e8e7419494887ee21b7aa9944232209846bc199
Cam Fortin <chamberlain.fortin@gmail.com>
2015-09-24 15:56:04 -0700

    removed insecure

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -2,9 +2,14 @@ Template.postSubmit.events({
 	'submit form': function(e) {
 		e.preventDefault();
 
+		var thisemail = "";
+
+		if (Meteor.user()) {thisemail = Meteor.user().emails[0].address} 
+
 		var post = {
 			url: $(e.target).find('[name=url]').val(),
-			title: $(e.target).find('[name=title]').val()
+			title: $(e.target).find('[name=title]').val(),
+			email: thisemail
 		};
 
 		post._id = Posts.insert(post);
