b00b0edc73ebfb5391c09f30f84c081a50cf6eb7
Cam Fortin <chamberlain.fortin@gmail.com>
2015-09-29 08:34:05 -0700

    sort posts by submitted

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,11 +1,11 @@
 Template.postsList.helpers({
   posts: function() {
-    return Posts.find();
+    return Posts.find({}, {sort: { submitted: -1}} );
   }
 });
 
 Template.post1.helpers({
   posts: function() {
-    return Posts.find({}, {sort: { title: -1 }} )
+    return Posts.find({}, {sort: { title: -1 }} );
   }
 });
